8252894bef2cceff542f984ce6da8636d2c086a6
BurningFlare <floopypiper@gmail.com>
2024-12-04 15:56:58 -0700

    hi

--- public_html/index.js
+++ public_html/index.js
@@ -1,10 +1,24 @@
+const {MongoClient} = require('mongodb')
+require('dotenv').config();
+const uri = process.env.MONGO_URI;
+const client = new MongoClient(uri,{});
 
-const mongoose = require("mongoose")
 console.log("hi");
-mongoose.connect("mongodb://localhost/pokemondb",{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(()=>{
-    console.log("connected")
-})
+async function run() {
+    try {
+        await client.connect();
+        console.log("Connected to MongoDB Atlas!");
+
+        const database = client.db('PokemonDB');
+        const collection = database.collection('pokemon');
+
+        const query = { name: "Bulbasaur" };
+        const user = await collection.findOne(query);
+        console.log("Queried document:", user);
+    } catch (err) {
+        console.error(err);
+    } finally {
+        await client.close();
+    }
+}
+run().catch(console.dir);
