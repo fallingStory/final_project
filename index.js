c2979d5a60d6485311bf749318ed14839b8beb69
dshe0602 <dshe0602@gmail.com>
2024-12-13 17:59:43 -0700

    PUSH TO GITHUB

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require('mongodb');
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 require('dotenv').config();
 
 const uri = process.env.MONGO_URI;
